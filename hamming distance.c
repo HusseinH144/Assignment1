b79a609a59b8af2ff63c73df817f6d491a093a04
HusseinH144 <7ussein7ammoud144@gmail.com>
2022-10-10 19:27:23 +0300

    Update hamming distance.c

--- hamming distance.c
+++ hamming distance.c
@@ -1,1 +1,22 @@
-
+int hammingDistance(int n1, int n2)
+{
+    int x = n1 ^ n2;
+    int setBits = 0;
+ 
+    while (x > 0) {
+        setBits += x & 1;
+        x >>= 1;
+    }
+ 
+    return setBits;
+}
+ 
+// Driver code
+int main()
+{   printf("Enter 2 integers with a space in between");
+    int x,y;
+    scanf(“%d  %d”, &x, &y);
+    printf("%d\n", hammingDistance(x, y));
+ 
+    return 0;
+}
