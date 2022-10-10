cfead198ade97e8ad2cb3057cd4a38a8e220e9af
HusseinH144 <7ussein7ammoud144@gmail.com>
2022-10-10 21:14:19 +0300

    Fixed Print Array and did print Histogram

--- my arrays.c
+++ my arrays.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
 #define SIZE 10 
-int printarray(int x[SIZE]);
+void printarray(int x[SIZE]);
+void printHistogram(int y[SIZE]);
 int main(){
     int array[SIZE] = {1,1,1,1,1,2,2,2,3,3 }; 
-    int a = printarray(array);
+     printarray(array);
+     printHistogram(array);
     return 0;
 }
-int printarray(int x[SIZE]){
+void printarray(int x[SIZE]){
     printf("Index Value\n");
     for(int i = 0; i < SIZE; i++)
         printf("%5d %5d \n",i,x[i]);
-    return 0;
+  
+}
+void printHistogram(int array[SIZE]) {
+    int results[SIZE] = {0};
+    for (int i = 0; i < SIZE; ++i) {   
+      for(int j = 0; j <SIZE; j++) {
+         if ( array[j] == i){
+            results[i]++;
+         }
+      } 
+   }
+    
+      for (int i = 0; i < SIZE; i++) {
+          if(results[i]!=0){
+      printf("[%d] ", i);
+      for ( int j = 0; j < results[i]; j++) {
+      printf("*");
+      }
+      printf("\n");}
+   }
 }
